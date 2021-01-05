f7fc4db2ece0d9420f3fdea923de0a210b083342
Iuliu Visovan <iuliu.visovan@usource.ro>
2021-01-05 23:35:21 +0200

    add muli font

--- screens/IssueDetailsScreen/index.tsx
+++ screens/IssueDetailsScreen/index.tsx
@@ -88,7 +88,17 @@ const getHtmlFromBody = (rawBody: string): string => {
     .replace(/##([^\r]+)/g, '<h3>$1</h3>')
     .replace(/`([^`]+)`/g, '<code>$1</code>')
     .replace(/`([^`]+)`/g, '<code>$1</code>');
-  const html = `<html><head><meta name="viewport" content="width=device-width, initial-scale=1.0"></head><body><p>${formattedBody}</p></body></html>`;
+  const html = `<html style="overflow: hidden">
+    <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="preconnect" href="https://fonts.gstatic.com">
+      <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;400;500&display=swap" rel="stylesheet">
+    </head>
+    <body style="font-family: Mulish; overflow: hidden">
+      <p>${formattedBody}</p>
+    </body>
+  </html>
+  `;
 
   return html;
 };
